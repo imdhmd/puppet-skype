ca8b9dbd8cb022d7ec28d360213dc9a7d79c7f30
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-05-06 10:52:12 -0300

    Fix tests

--- spec/classes/skype_spec.rb
+++ spec/classes/skype_spec.rb
@@ -4,7 +4,7 @@ describe 'skype' do
   it do
     should contain_package('Skype').with({
       :provider => 'appdmg',
-      :source   => 'http://download.skype.com/macosx/Skype_6.12.60.431.dmg',
+      :source   => 'http://download.skype.com/macosx/Skype_6.15.60.330.dmg',
     })
   end
 end
